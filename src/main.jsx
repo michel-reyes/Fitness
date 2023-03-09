9cc0c10019845e2ee992b763ca30e60d3f9183bb
Michel Reyes <mreyesh85@gmail.com>
2023-03-09 17:38:44 -0500

    change to hash routes since gh-pages is unable to work ok on reloads

--- src/main.jsx
+++ src/main.jsx
@@ -3,36 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App, Page } from 'konsta/react';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { Root } from '@/routes/Root';
 import { ErrorPage } from '@/routes/ErrorPage';
 import { Dashboard, Beginner } from '@/routes/xero';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: '/fitness/',
+    path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
   },
   {
-    path: '/fitness/xero',
+    path: 'xero',
     element: <Dashboard />,
     errorElement: <ErrorPage />,
   },
   {
-    path: '/fitness/xero/beginner',
+    path: 'xero/beginner',
     element: <Beginner />,
   },
   {
-    path: '/fitness/xero/intermediate',
+    path: 'xero/intermediate',
     element: <p>Xero2 for intermediate</p>,
   },
   {
-    path: '/fitness/xero/advanced',
+    path: 'xero/advanced',
     element: <p>Xero2 for advanced</p>,
   },
   {
-    path: '/fitness/hitt',
+    path: 'hitt',
     element: <p>HIIT</p>,
   },
 ]);
@@ -41,10 +41,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App theme="material">
       <Page>
-        <RouterProvider
-          basename="https://michel-reyes.github.io/fitness/"
-          router={router}
-        />
+        <RouterProvider router={router} />
       </Page>
     </App>
   </React.StrictMode>,
