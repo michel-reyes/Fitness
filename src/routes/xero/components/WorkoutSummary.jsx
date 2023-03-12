2ab87ae58627533fe8f880168a07d93a5b2e62ee
Michel Reyes <mreyesh85@gmail.com>
2023-03-12 14:35:04 -0400

    Change summary text size

--- src/routes/xero/components/WorkoutSummary.jsx
+++ src/routes/xero/components/WorkoutSummary.jsx
@@ -6,14 +6,14 @@ function WorkoutSummary() {
     currentDay: { workouts },
   } = useBeginnerContext();
 
-  console.log({ workouts });
-
   return (
     <div className="my-32 mt-40 px-6">
-      <h2 className="my-4 text-lg">Summary</h2>
+      <h2 className="my-4 text-xl">Summary</h2>
+
       <ul className="list-none p-0">
         {workouts.map(({ title, image, sets, time, reps }) => {
           const rounds = fixRounds(sets, time, reps);
+
           return (
             <li className="flex p-3 pl-0 items-center" key={title}>
               <div className="mr-4 border-slate-800 border-2 max-w-sm rounded-md">
